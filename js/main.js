3e6cb7400828460d97704d1b51af3c86f48b7795
uzwork <89359675+uzwork@users.noreply.github.com>
2023-01-14 19:24:17 +0500

    derfg
    dfg

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-var loc = alert("Manzil pastda location down местоположение вниз")
+// const myTimeout = setTimeout(myGreeting, 5000);
+// var oo = document.querySelector("body")
+// var red = document.querySelector("#boy");
+
+// function myGreeting() {
+
+//     var loc = confirm("Manzil pastda location down местоположение вниз")
+//     if (loc == true) {
+//         // oo.setAttribute("href", "#red");
+//         red.setAttribute("href", "#red")
+//         red.addEventListener("click", confirm('clicked'), true);
+//     }
+
+//     var element = document.getElementsByClassName('classname');
+
+
+// }
